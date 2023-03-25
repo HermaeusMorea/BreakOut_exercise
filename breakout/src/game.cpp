8fc722248b15b934282b8c3d4df0b12c91ef631f
xiayu <23102052+HermaeusMorea@users.noreply.github.com>
2023-03-25 15:38:20 +0800

    Add some comments where performance can be optimized.

--- breakout/src/game.cpp
+++ breakout/src/game.cpp
@@ -29,7 +29,8 @@ void Game::Init()
     ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
     // set render-specific controls
     Shader shader = ResourceManager::GetShader("sprite");
-    Renderer = new SpriteRenderer(shader);
+    Renderer = new SpriteRenderer( shader );
+    //Renderer = new SpriteRenderer( ResourceManager::GetShader("sprite") );
     // load textures
     ResourceManager::LoadTexture("textures/awesomeface.png", true, "face");
 }
@@ -49,4 +50,5 @@ void Game::Render()
     Texture2D texture;
     texture = ResourceManager::GetTexture("face");
     Renderer->DrawSprite(texture, glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+    //Renderer->DrawSprite(ResourceManager::GetTexture("face"), glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
