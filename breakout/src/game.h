33d50042218e9a0462aa0348f14e049c0c30a949
xiayu <23102052+HermaeusMorea@users.noreply.github.com>
2023-03-26 09:55:18 +0800

    finishing the game's main body.

--- breakout/src/game.h
+++ breakout/src/game.h
@@ -13,10 +13,24 @@ enum GameState {
     GAME_WIN
 };
 
+// Represents the four possible (collision) directions
+enum Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
+// Defines a Collision typedef that represents collision data
+typedef std::tuple<bool, Direction, glm::vec2> Collision; // <collision?, what direction?, difference vector center - closest point>
+
 // Initial size of the player paddle
 const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
 // Initial velocity of the player paddle
 const float PLAYER_VELOCITY(500.0f);
+// Initial velocity of the Ball
+const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
+// Radius of the ball object
+const float BALL_RADIUS = 12.5f;
 
 // Game holds all game-related state and functionality.
 // Combines all game-related data into a single class for
@@ -30,7 +44,6 @@ public:
     unsigned int            Width, Height;
     std::vector<GameLevel>  Levels;
     unsigned int            Level;
-    void DoCollisions();
     // constructor/destructor
     Game(unsigned int width, unsigned int height);
     ~Game();
@@ -40,6 +53,10 @@ public:
     void ProcessInput(float dt);
     void Update(float dt);
     void Render();
+    void DoCollisions();
+    // reset
+    void ResetLevel();
+    void ResetPlayer();
 };
 
 #endif
